0c04c5f5b7a30b538c969173deb19babf23a9600
victoriatrac <victoriatrac@gmail.com>
2022-09-20 17:03:49 -0600

    made skeleton of front page

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+import portrait from './img/portrait.jpg'
 
 function App() {
+  const [data, setData] = React.useState(null)
+
+  React.useEffect(() => {
+    fetch('/api/project')
+      .then((res) => res.json())
+      .then((data) => setData(data.message))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+
+        <h1>Hi! I'm Victoria Trac.</h1>
+
+        <img src={portrait} alt='me smiling against a wall of colorful artwork' className='portrait'/> 
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I created this Linktree-style page specifically for the Denver Startup Week 2022 job fair. It uses React, Express, and a serverless Node API. Hosted on Vercel.
+          <br /><a href='https://github.com/victoriatrac/portfolio-2022-fe'>GitHub - front end</a>
+          <br /><a href='https://github.com/victoriatrac/portfolio-2022-be'>GitHub - back end</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button>Button 1</button>
+        <button>Button 1</button>
+        <button>Button 1</button>
+
+        <p>{ !data ? 'Loading...' : data }</p>
+
       </header>
     </div>
   );
